358d06ff873430d6ad2d6d6158dcafa41ef281c3
David Teixeira de Masin <davidteixeira.info@gmail.com>
2022-02-09 15:12:45 -0300

    Desafios realizados no curso da Chronos Academy

---
+++ DesafioAula02/index.js
@@ -0,0 +1,8 @@
+function calcTriangleArea() {
+  const base = baseTriangulo.value
+  console.log(base)
+  const altura = alturaTriangulo.value
+  console.log(altura)
+  area = (base * altura) / 2
+  resultado.value = area
+}
